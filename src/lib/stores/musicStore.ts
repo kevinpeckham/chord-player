9f664a6af9e795d7471e8b30a9b87ad7b9bd4ce5
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-08-21 15:28:39 -0400

    add store for chord data

---
+++ src/lib/stores/musicStore.ts
@@ -0,0 +1,35 @@
+// store function
+import { readable } from "svelte/store";
+
+// types
+import type { ChordDatum } from "$types/types";
+
+import { default as circleRaw } from "$data/circle-of-fifths-data.json";
+import { default as notesRaw } from "$data/notes.json";
+import { default as chordsRaw } from "$data/chords.json";
+
+const circle: ChordDatum[] = circleRaw;
+const notes: { [key: string]: number } = notesRaw;
+const chords: { [key: string]: string[] } = chordsRaw;
+
+const derived = circle.map(chord => {
+  const majorId = chord.majorId;
+  const majorNotes = chords[majorId];
+  const majorFrequencies = majorNotes.map(note => notes[note]);
+  const minorId = chord.minorId;
+  const minorNotes = chords[minorId];
+  const minorFrequencies = minorNotes.map(note => notes[note]);
+
+  return {
+    ...chord,
+    majorNotes,
+    majorFrequencies,
+    minorNotes,
+    minorFrequencies,
+  }
+
+
+})
+
+
+export const chordsStore = readable(derived);
