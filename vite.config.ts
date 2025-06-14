fb907fa68675936f0acc6fe7f7358d3d13475d51
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2025-06-13 23:08:59 -0400

    chore: replace vitest/config with vite import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import UnoCSS from "unocss/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [UnoCSS(), sveltekit()],
